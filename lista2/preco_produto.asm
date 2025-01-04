d71e0b4e8f1f715b67eccc337e08b648d91dba29
CaioDuart3 <caioduarte330@gmail.com>
2025-01-04 00:57:53 -0300

    exercícios de float/double adicionados

---
+++ lista2/preco_produto.asm
@@ -0,0 +1,33 @@
+.data
+    cem: .double 100.00
+.text
+main: 
+
+    l.d $f8, cem
+
+    li $v0, 7
+    syscall
+    mov.d $f2, $f0
+
+    li $v0, 7
+    syscall
+    mov.d $f4, $f0
+
+    li $v0, 7
+    syscall
+    mov.d $f6, $f0
+
+    # calcula o valor equivalente em x% do produto
+    div.d $f10, $f4, $f8 
+    mul.d $f14, $f10, $f2 
+
+    # calcula o valor equivalente em y% do produto
+    div.d $f10, $f6, $f8 
+    mul.d $f16, $f10, $f2 
+
+    # soma valor de x% + valor de y% + valor real do produto.
+    add.d $f12, $f14, $f16
+    add.d $f12, $f12, $f2
+
+    li $v0, 3
+    syscall
