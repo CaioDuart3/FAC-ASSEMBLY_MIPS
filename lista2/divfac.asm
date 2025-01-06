da67f64011973a13af986efcc10dd5ca282f3522
CaioDuart3 <caioduarte330@gmail.com>
2025-01-06 09:55:43 -0300

    adicionando comentários

--- lista2/divfac.asm
+++ lista2/divfac.asm
@@ -1,15 +1,28 @@
+# *passo1: dividendo em R[31...0]
+# *passo2: R << 1
+# *passo3: R[63...32] = R[63...32] - divisor
+# *passo4: 
+#todo         4.1: se R >=0, R << 1 e R++
+#todo         4.2: se R < 0, R[63...32] = R[63...32] + divisor e R << 1
+# *passo5: se não rodou 31 vezes, volte para o passo 3 
+# *passo6: R[63...31] >> 1
+
 .text
 main:
+    #lê o dividendo (numerador)
     li $v0, 5
     syscall
     move $a0, $v0
 
+    #lê o divisor (denominador)
     li $v0, 5
     syscall
     move $a1, $v0
 
+    #chama a função que realiza o algoritimo de divisão
     jal divfac
 
+#após a chamada da função realiza a impressão para testes:
 mfhi $a1
 mflo $a0
 
@@ -32,48 +45,67 @@ li $v0,10
 syscall
 
     divfac:
+        #se o dividendo < 0, t0=1
         slt $t0, $a0, $zero 
+        #se o numerador < 0, t1=1
         slt $t1, $a1, $zero 
 
+        #se dividendo>0, verifica o outro numero
         beq $t0, $zero, verificarD
+        #se dividendo<0, torna-o positivo
         nor $a0, $a0, $zero
         addi $a0, $a0, 1
     
     verificarD:
+        #se divisor>0, inicia a lógica
         beq $t1, $zero, passo1
+        #se divisor<0, torna-o positivo
         nor $a1, $a1, $zero
         addi $a1, $a1, 1
 
     passo1: 
+        # contador
         li $t9, 32
-        move $s1, $zero
-        move $s0, $a0 
+        move $s1, $zero #R[63...32] =0
+        move $s0, $a0 #R[31...0] = dividendo
 
     passo2:
+        #move R[63...0] << 1, logo R << 1
         sll $s0, $s0, 1
         sll $s1, $s1, 1 
 
     passo3:
+        #R[63...32] = R[63...32] - divisor
         sub $s1, $s1, $a1
 
     passo4:
+        #se R < 0, t3 = 0
         slt $t3, $s1, $zero
-        bne $t3, $zero, passo4_1
+        # se t3 = 1, realiza passo 4.2, pois R<0
+        bne $t3, $zero, passo4_2
+        # se t3 = 0, realiza passo 4.1, pois R>=0
 
+        # *passo4.1
+        #lógica para verifica e realizar a troca de 1 de lo para hi
         li $t5, 1
         sll $t5, $t5, 31
         and $t5, $s0, $t5
+        #faz o deslocamento R<<1 para realizar a troca corretamente
         sll $s0, $s0, 1
         sll $s1, $s1, 1
-        srl $t5, $t5, 31 
+        srl $t5, $t5, 31
         add $s1, $s1, $t5
 
+        # R[0] = 1 
         addi $s0, $s0, 1
         j passo5
 
-    passo4_1:
+    passo4_2:
+        # R[63...32] = R[63...32] + divisor
         add $s1, $s1, $a1
 
+        # R << 1
+        #lógica para verifica e realizar a troca de 1 de lo para hi
         li $t5, 1
         sll $t5, $t5, 31
         and $t5, $s0, $t5
@@ -85,16 +117,19 @@ syscall
         j passo5
 
     passo5:
-        addi $t9, $t9, -1
+        addi $t9, $t9, -1 #contador --, se contador > 0, volte para o passo 3
         beq $t9, $zero, passo6
         j passo3
 
     passo6:
+        # R[63...32] >> 1
         srl $s1, $s1, 1
         j verificarSinal
 
     verificarSinal:
+        # se os sinais são diferentes, negativa o quociente
         bne $t1, $t0, negativarQuociente
+        # se dividendo < 0, negativa resto
         bne $t0, $zero, negativarResto
         j fim
 
--- lista2/multfac.asm
+++ lista2/multfac.asm
@@ -1,63 +1,77 @@
+# *passo1: Inicialize com P[63 . . . 32] = 0, P[31 . . . 0] = Q
+# *passo2: Se P[0] = 1, P[63 . . . 32] + = M
+# *passo3: P >> 1
+# *passo4: Se não exceder 32 iterações, volte ao passo 2.
+
 .text
 main:
+    #lê M
     li $v0,5
     syscall
     move $a0, $v0
-
+    #lê Q
     li $v0,5
     syscall
     move $a1, $v0
 
+    #chamada da função que inicia o algoritimo de multiplicação
     jal multfac
 
-    mfhi $a0
-    mflo $a1
+#após o retorno da função é realizado a impressão do resultado para testes:
+mfhi $a0
+mflo $a1
 
-    li $v0, 1
-    syscall
+li $v0, 1
+syscall
 
-    li $v0, 11
-    li $a0, 10
-    syscall
-    
-    li $v0, 1
-    move $a0, $a1
-    syscall
+li $v0, 11
+li $a0, 10
+syscall
 
-    li $v0, 11
-    li $a0, 10
-    syscall
+li $v0, 1
+move $a0, $a1
+syscall
 
-    li $v0,10
-    syscall
+li $v0, 11
+li $a0, 10
+syscall
+
+li $v0,10
+syscall
 
     multfac:
 
-        li $t9, 32
-        slt $t0, $a0, $zero
-        slt $t1, $a1, $zero
+        li $t9, 32 # contador 
+        slt $t0, $a0, $zero  # M < 0
+        slt $t1, $a1, $zero # Q < 0
 
-
+        # se M>0, t0 = 0
         beq $t0, $zero, verificarM
+        # se M<0, transforma em positivo
         nor $a0, $a0, $zero
         addi $a0,$a0, 1
 
     verificarM:
-
+        # se Q>0, t1 = 0
         beq $t1, $zero, passo1
+        # se Q<0, transforma em positivo
         nor $a1, $a1, $zero
         addi $a1,$a1, 1
-
+    
     passo1: 
-        move $s0, $zero
-        move $s1, $a1
+        move $s0, $zero # P[64...32] = Q
+        move $s1, $a1 #P[31...0] = 0
 
     passo2:
+        # extrai P[0]
         andi $t3, $s1, 1 
+        # verifica se P[0] = 1, se sim, ele pula beq e soma, se não, pula pra P >> 1
         beq $t3, $zero, passo3
+        # como P[0] = 1, P[63 . . . 32] + = M
         add $s0, $s0, $a0
 
     passo3:
+        # lógica que verifica se o bit de hi vai pra lo, e faz a troca
         andi $t2, $s0, 1 
         srl $s1, $s1, 1 
         srl $s0, $s0, 1
@@ -65,17 +79,21 @@ main:
         sll $t2, $t2, 31
         add $s1, $s1, $t2 
 
-        addi $t9, $t9, -1 
+        # fim do passo 3
+        addi $t9, $t9, -1  #contador--
         beq $t9, $zero, verificarSinal
+        # passo4: Se não exceder 32 iterações, volte ao passo 2.
         j passo2
 
     verificarSinal: 
+        #troca o sinal do produto caso necessário
         beq $t0, $t1, fim 
         nor $s0, $s0, $zero 
         nor $s1, $s1, $zero
         addi $s1, $s1, 1
     
     fim: 
+        #armazena o produto em hi e lo
         mthi $s0
         mtlo $s1
         jr $ra
